6d77b58a0f392d34790f01094bd3acf7e8cf2437
Chris Ackerman <chris@topher.land>
2022-08-11 20:59:13 -0700

    updated args parser

--- jest.config.cjs
+++ jest.config.cjs
@@ -13,15 +13,18 @@ module.exports = {
   coverageProvider: 'v8',
   coverageReporters: ['text-summary', 'html-spa', 'lcov'],
   coverageThreshold: { global: { branches: 50, functions: 50, lines: 50, statements: 50 } },
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  globals: {
+    'ts-jest': { useESM: true },
+  },
   moduleNameMapper: {
     // Remove the .js extension (required for ES Module support) from TS file imports.
-    '(.*)\\.js$': '$1',
+    '(.*)\\.jsx?$': '$1',
   },
   preset: 'ts-jest',
   restoreMocks: true,
   roots: ['src'],
   setupFilesAfterEnv: [],
   testEnvironment: 'node',
-  transformIgnorePatterns: ['/node_modules/core-js/'],
   verbose: true,
 };
--- src/util/args.ts
+++ src/util/args.ts
@@ -1,125 +1,136 @@
-type UnionToIntersection<T> = (T extends any ? (x: T) => any : never) extends (x: infer V) => any ? V : never;
-type Simplify<T> = any extends any ? { readonly [P in keyof T]: T[P] } : never;
-type ArgsType<TReturn = unknown> = (value: string) => TReturn;
-type ArgsOptions<TOptions extends Record<string, ArgsType>> = {
-  readonly [P in keyof TOptions]: P extends `${'-'}${string}` | `${string}${'='}${string}` ? never : TOptions[P];
+type ArgParser<TValue> = (arg: string) => TValue;
+type Args<TParsers extends Record<string, ArgParser<any>>> = {
+  readonly get: <TKey extends keyof TParsers>(arg: TKey) => ReturnType<TParsers[TKey]> | undefined;
+  readonly getAll: <TKey extends keyof TParsers>(arg: TKey) => readonly ReturnType<TParsers[TKey]>[];
+  readonly getRequired: <TKey extends keyof TParsers>(arg: TKey) => ReturnType<TParsers[TKey]>;
+  readonly has: (arg: keyof TParsers) => boolean;
+  readonly positional: readonly string[];
 };
-type ArgsAliases<TOptionNames, TAliases extends Record<string, TOptionNames>> = {
-  readonly [P in keyof TAliases]: P extends `${'-'}${string}` | `${string}${'='}${string}` ? never : TAliases[P];
-};
-type Args<TOptions extends ArgsOptions<any>> = Simplify<
-  UnionToIntersection<
-    {
-      readonly [P in keyof TOptions]: TOptions[P] extends ArgsType<infer TType>
-        ? {
-            readonly get: (arg: P) => TType | undefined;
-            readonly getAll: (arg: P) => readonly TType[];
-            readonly getRequired: (arg: P) => TType;
-            readonly has: (arg: P) => boolean;
-          }
-        : never;
-    }[keyof TOptions]
-  > & { readonly positional: readonly string[] }
->;
 
 const parseArgs = <
-  TOptions extends Record<`${'-'}${string}` | `${string}${'='}${string}`, never> & Record<string, ArgsType>,
-  TAliases extends Record<`${'-'}${string}` | `${string}${'='}${string}`, never> & Record<string, keyof TOptions> = {},
+  TParsers extends Record<keyof TParsers, ArgParser<any> | BooleanConstructor>,
+  TAliases extends Record<keyof TAliases, keyof TParsers>,
 >(
   argv: readonly string[],
-  options: ArgsOptions<TOptions>,
-  aliases: ArgsAliases<keyof TOptions, TAliases> = {} as ArgsAliases<keyof TOptions, TAliases>,
-): Args<ArgsOptions<TOptions>> => {
-  const argvCopy = [...argv];
-  const positional: string[] = [];
-  const args: Record<string, any[]> = {};
+  parsers: TParsers,
+  aliases: TAliases = {} as TAliases,
+): Args<TParsers> => {
+  argv = [...argv];
 
-  for (const [key] of Object.entries(options) as [string, ArgsType][]) {
-    args[key] = [];
-  }
+  const options: { [P in keyof TParsers]?: { name: P; parse: TParsers[P] } } = Object.create(null);
+  const aliasOptions: { [P in keyof TAliases]?: { name: TAliases[P]; parse: TParsers[TAliases[P]] } } =
+    Object.create(null);
+  const values: {
+    [P in keyof TParsers]?: readonly ReturnType<TParsers[P]>[];
+  } = Object.create(null);
+  const positional: string[] = [];
+
+  (Object.getOwnPropertyNames(parsers) as (keyof TParsers)[]).forEach((key) => {
+    options[key] = { name: key, parse: parsers[key] };
+  });
+  (Object.getOwnPropertyNames(aliases) as (keyof TAliases)[]).forEach((key) => {
+    aliasOptions[key] = { name: aliases[key], parse: parsers[aliases[key]] };
+  });
+
+  const nextArg = (): string | undefined => {
+    const value = argv.at(0);
+    argv = argv.slice(1);
+    return value;
+  };
+
+  const addValue = <TKey extends keyof TParsers>(name: TKey, value: ReturnType<TParsers[TKey]>): void => {
+    values[name] = [...(values[name] ?? []), value];
+  };
 
   let arg: string | undefined;
 
-  while ((arg = argvCopy.shift()) != null) {
+  while ((arg = nextArg()) != null) {
     if (arg === '--') {
-      positional.push(...argvCopy);
+      positional.push(...argv);
       break;
     }
 
-    const match = arg.match(/^(?<dashes>-{1,2})(?<name>[^=]*)(?:=(?<value>.*))?$/su);
+    const match = arg.match(/^(?<dashes>-{1,2})(?<name>[^=]*)(?:=(?<maybeValue>.*))?$/su);
 
-    if (!match?.groups) {
+    if (!match?.groups || match.groups.dashes == null || match.groups.name == null) {
       positional.push(arg);
       continue;
     }
 
-    const dashes = match.groups.dashes;
-    let name = match.groups.name;
-    let value = match.groups.value as string | undefined;
+    const { dashes, name, maybeValue } = match.groups;
 
-    if (dashes === '-' && /^.{2}/u.test(name)) {
+    if (dashes === '-') {
       const chars = Array.from(name);
 
       if (chars.length > 1) {
-        if (value != null) {
-          argvCopy.unshift(value);
+        if (maybeValue != null) {
+          argv = [maybeValue, ...argv];
         }
 
-        argvCopy.unshift(...chars.filter((char) => char !== '-').map((char) => `-${char}`));
+        argv = [...chars.filter((char) => char !== '-').map((char) => `-${char}`), ...argv];
         continue;
       }
     }
 
-    if (!(name in options) && name in aliases) {
-      name = (aliases as Record<string, string>)[name];
-    }
+    const option = options[name as keyof TParsers] ?? aliasOptions[name as keyof TAliases];
 
-    if (!(name in options)) {
+    if (!option) {
       throw new Error(`Option ${JSON.stringify(arg)} is unknown`);
     }
 
-    const parse = (options as Record<string, ArgsType>)[name];
-
-    if (parse === Boolean) {
-      if (value != null) {
+    if (option.parse === Boolean) {
+      if (maybeValue != null) {
         throw new Error(`Option ${JSON.stringify(arg)} does not accept a value`);
       }
 
-      args[name].push(true);
+      addValue(option.name, true as ReturnType<typeof option.parse>);
       continue;
     }
 
-    if (value == null) {
-      value = argvCopy.shift();
+    const value = maybeValue ?? nextArg();
 
-      if (value == null) {
-        throw new Error(`Option ${JSON.stringify(arg)} requires a value`);
-      }
+    if (value == null) {
+      throw new Error(`Option ${JSON.stringify(arg)} requires a value`);
     }
 
-    args[name].push(parse(value));
+    const parsedValue = (() => {
+      try {
+        return option.parse(value);
+      } catch (error: any) {
+        throw new Error(`Option ${JSON.stringify(arg)} value is invalid.\n${error?.message || error}`);
+      }
+    })();
+
+    if (parsedValue == null) {
+      throw new Error(`Option ${JSON.stringify(arg)} value is invalid`);
+    }
+
+    addValue(option.name, parsedValue);
   }
 
-  return {
-    get: (key: string) => {
-      return args[key].at(-1);
+  const args = {
+    get: <TKey extends keyof TParsers>(key: TKey): ReturnType<TParsers[TKey]> | undefined => {
+      return values[key]?.at(-1);
     },
-    getAll: (key: string) => {
-      return args[key] ?? [];
+    getAll: <TKey extends keyof TParsers>(key: TKey): ReturnType<TParsers[TKey]>[] => {
+      return [...(values[key] ?? [])];
     },
-    getRequired: (key: string) => {
-      const values = args[key];
-      if (!values?.length) {
+    getRequired: <TKey extends keyof TParsers>(key: TKey): ReturnType<TParsers[TKey]> => {
+      const value = args.get(key);
+
+      if (value == null) {
         throw new Error(`Missing required argument ${JSON.stringify(key)}`);
       }
 
-      return values;
+      return value;
     },
-    has: (key: string) => {
-      return args[key]?.length > 0;
+    has: (key: keyof TParsers): boolean => {
+      return Boolean(values[key]?.length);
     },
     positional,
-  } as unknown as Args<ArgsOptions<TOptions>>;
+  };
+
+  return args;
 };
 
-export { parseArgs };
+export { type ArgParser, type Args, parseArgs };
